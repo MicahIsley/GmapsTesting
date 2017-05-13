5e3966cd4f467770e380b608b17cbf302e0ba58b
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-05-13 11:01:20 -0400

    asdf

--- logic.js
+++ logic.js
@@ -1,20 +1,31 @@
   var config = {
-    apiKey: "AIzaSyAa0IsOucOudM_yR0ql8IPcxwgn3TiN_gQ",
-    authDomain: "bonapetit-df088.firebaseapp.com",
-    databaseURL: "https://bonapetit-df088.firebaseio.com",
-    projectId: "bonapetit-df088",
-    storageBucket: "bonapetit-df088.appspot.com",
-    messagingSenderId: "1001242375810"
+    apiKey: "AIzaSyAtzmg1dG3fWTrispYckD18L0Wnz0RAGno",
+    authDomain: "bonapetite-ffb65.firebaseapp.com",
+    databaseURL: "https://bonapetite-ffb65.firebaseio.com",
+    projectId: "bonapetite-ffb65",
+    storageBucket: "bonapetite-ffb65.appspot.com",
+    messagingSenderId: "695948248134"
   };
   firebase.initializeApp(config);
 
 var database = firebase.database();
 
 var favRestaurants = [];
+var wantToGo = [];
+var person;
+var restaurants = [];
 
-
-var map, infoWindow;
+//pull all the nessecary information from the database
+database.ref().on("value", function(snapshot) {
+    snapshot.forEach(function(child){
+      var key = child.key;
+      restaurants = child.val().restaurants;
+      console.log(restaurants);
+    });
+    //name = person.Preference.restaurantName
+})
       function initMap() {
+        var iconBase = 'https://maps.google.com/mapfiles/kml/paddle/';
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 13
@@ -32,7 +43,15 @@ var map, infoWindow;
             var marker = new google.maps.Marker({
               position: pos,
               map: map
-        });
+            });
+            for(i=0; i < favRestaurants.length; i++) {
+              var randPos = {lat: favRestaurants[i].lat, lng: favRestaurants[i].lng};
+              var marker =  new google.maps.Marker({
+                position: randPos,
+                icon: iconBase + 'grn-stars.png',
+                map: map
+              });
+            }  
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
@@ -51,29 +70,27 @@ var map, infoWindow;
       }
 
 
+//make a function to pull all of the favorite restaurants from the database and push them into an array.
 
-function addFavoriteRestaurant() {
-  database.ref().on("value", function(snapshot) {
-      name = snapshot.val().Person.Preference.restaurantName;
-      lat = snapshot.val().Person.Preference.location.latitude;
-      lng = snapshot.val().Person.Preference.location.longitude;
+function addToLists() {
+  for(i=0; i < restaurants.length; i++){
+    var name = restaurants[i].name;
+    var lat = restaurants[i].location.latitude;
+    var lng = restaurants[i].location.longitude;
     var locationObject = {name:name, lat:lat, lng:lng};
-    favRestaurants.push(locationObject);
-  })
+    if(restaurants.type == 0) {
+      favRestaurants.push(locationObject);
+    } else {
+      wantToGo.push(locationObject);
+    }
+  }  
 }
 $(document).ready(function() {
 
-function addMarker() {
-  var marker2 = new google.maps.Marker({
-          position: {lat: 132, lng: 132},
-          map: map
-        });
-}
-addMarker();
-addFavoriteRestaurant();
+addToLists();
 console.log(favRestaurants);
 console.log("hi");
-
+console.log(restaurants);
 })
 
 
